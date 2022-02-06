a15eda0b6364015a41e3a19f5486df357799d73e
projxit <kelvin@projx.it>
2022-02-06 02:23:44 +0000

    Build Me

--- pwrmgt.py
+++ pwrmgt.py
@@ -29,36 +29,3 @@ if __name__ == '__main__':
     main.add_command(host_args.host)
     main()
 
-
-
-200
-200
-Traceback (most recent call last):
-  File "/app/pwrmgt.py", line 30, in <module>
-    main()
-  File "/usr/local/lib/python3.10/site-packages/click/core.py", line 1128, in __call__
-    return self.main(*args, **kwargs)
-  File "/usr/local/lib/python3.10/site-packages/click/core.py", line 1053, in main
-    rv = self.invoke(ctx)
-  File "/usr/local/lib/python3.10/site-packages/click/core.py", line 1659, in invoke
-    return _process_result(sub_ctx.command.invoke(sub_ctx))
-  File "/usr/local/lib/python3.10/site-packages/click/core.py", line 1659, in invoke
-    return _process_result(sub_ctx.command.invoke(sub_ctx))
-  File "/usr/local/lib/python3.10/site-packages/click/core.py", line 1395, in invoke
-    return ctx.invoke(self.callback, **ctx.params)
-  File "/usr/local/lib/python3.10/site-packages/click/core.py", line 754, in invoke
-    return __callback(*args, **kwargs)
-  File "/app/lib/host_args.py", line 144, in up
-    vm_runner.taskmgr.wait(True)
-  File "/app/lib/vmware.py", line 62, in wait
-    raise e
-  File "/app/lib/vmware.py", line 59, in wait
-    task.WaitForTasks(tasks=self.tasks, onProgressUpdate=progress_call)
-  File "/usr/local/lib/python3.10/site-packages/pyVim/task.py", line 216, in WaitForTasks
-    raise err
-pyVmomi.VmomiSupport.vim.fault.InvalidState: (vim.fault.InvalidState) {
-   dynamicType = <unset>,
-   dynamicProperty = (vmodl.DynamicProperty) [],
-   msg = 'The operation is not allowed in the current state.',
-   faultCause = <unset>,
-   faultMessage = (vmodl.LocalizableMessage) []
